883b0d48e747671c2563d48d5854fe46bfa5e245
karan suthar <karan.suthar.lakha@gmail.com>
2025-08-20 14:59:54 +0530

    fix: update compileSdk and ndkVersion to specific values for firebase compatibility

--- chat_app/android/app/build.gradle.kts
+++ chat_app/android/app/build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
 
 android {
     namespace = "works.karansuthar.chat_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+    compileSdk = 36  
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,8 +27,8 @@ android {
         applicationId = "works.karansuthar.chat_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
