887d3e4725f8d4470645529fe2e51aadda11f447
karan suthar <karan.suthar.lakha@gmail.com>
2025-08-20 19:20:51 +0530

    feat: create MainPage widget with basic layout

---
+++ chat_app/lib/screens/main/main_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Main Page",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
